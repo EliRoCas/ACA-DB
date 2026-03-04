bcdfc642e06d912999546786e2a9833ca24d8719
Eli RoCas <elir_94@hotmail.com>
2026-03-04 12:43:46 -0500

    move: landing a la carpeta raiz Templates

--- memberships/views.py
+++ memberships/views.py
@@ -21,7 +21,7 @@ class StaffRequiredMixin(UserPassesTestMixin):
 
 # Create your views here.
 class LandingPageView(TemplateView):
-    template_name = 'memberships/landing.html'
+    template_name = 'landing.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
